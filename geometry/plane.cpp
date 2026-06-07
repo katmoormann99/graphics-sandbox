39702f5c1edfef092c9f53739ab30876cba90a1f
Kat Moormann <kathryn.moormann@jhuapl.edu>
2026-06-07 16:47:15 -0400

    5 methods within plane.cpp

--- geometry/plane.cpp
+++ geometry/plane.cpp
@@ -13,29 +13,44 @@ Plane::Plane(const Point3 &p, const Vector3 &n) { set(p, n); }
 
 Plane::Plane(const Point3 &p1, const Point3 &p2, const Point3 &p3)
 {
-    // Student to define - Module 1
+    Vector3 v1(p1, p2);
+    Vector3 v2(p1, p3);
+
+    Vector3 n = v1.cross(v2);
+    set(p1, n);
 }
 
 void Plane::set(const Point3 &p, const Vector3 &n)
 {
-    // Student to define - Module 1
+    a = n.x;
+    b = n.y; 
+    c = n.z;
+
+    // ax + by + cz + d = 0
+    d = -(a * p.x + b * p.y + c * p.z);
 }
 
 void Plane::normalize()
 {
-    // Student to define - Module 1
+    float mag = std::sqrt(a * a + b * b + c * c);
+
+    if (mag != 0.0f)
+    {
+        a /= mag;
+        b /= mag;
+        c /= mag;
+        d /= mag;
+    }
 }
 
 float Plane::solve(const Point3 &p) const
 {
-    // Student to define - Module 1
-    return 0.0f;
+    return a * p.x + b * p.y + c * p.z + d;
 }
 
 Vector3 Plane::get_normal() const
 {
-    // Student to define - Module 1
-    return Vector3();
+    return Vector3(a, b, c);
 }
 
 } // namespace cg
